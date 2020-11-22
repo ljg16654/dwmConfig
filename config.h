d17b23de344cfd115654c7db7d7ac6987cae16dd
Jigang Li <ljg16654@sjtu.edu.cn>
2020-11-22 18:24:01 +0800

    added patches: actualFullScreen and tatami, removed Super+R for ranger

--- config.h
+++ config.h
@@ -28,17 +28,14 @@ typedef struct {
 const char *spcmd1[] = {"st", "-n", "spterm", "-g", "80x34", NULL};
 const char *spcmd2[] = {"st",    "-n", "calc",    "-g",
                         "80x34", "-e", "wolfram", NULL};
-const char *spcmd3[] = {"st",    "-n", "rangerfm", "-g",
-                        "80x34", "-e", "ranger",   NULL};
-const char *spcmd4[] = {"electron-ssr", NULL};
+const char *spcmd3[] = {"electron-ssr", NULL};
 const char *eccmd[] = {"emacsclient", "-c", NULL};
 
 static Sp scratchpads[] = {
     /*  name         cmd */
     {"term", spcmd1},
     {"wolfram", spcmd2},
-    {"fm", spcmd3},
-    {"vpn", spcmd4},
+    {"vpn", spcmd3},
 };
 
 /* tagging */
@@ -55,8 +52,7 @@ static const Rule rules[] = {
     {"Firefox", NULL, NULL, 1 << 8, 0, -1},
     {NULL, "spterm", NULL, SPTAG(0), 1, -1},
     {NULL, "calc", NULL, SPTAG(1), 1, -1},
-    {NULL, "rangerfm", NULL, SPTAG(2), 1, -1},
-    {"electron-ssr", NULL, NULL, SPTAG(3), 1, -1},
+    {"electron-ssr", NULL, NULL, SPTAG(2), 1, -1},
 };
 
 /* layout(s) */
@@ -65,11 +61,13 @@ static const int nmaster = 1;    /* number of clients in master area */
 static const int resizehints =
     1; /* 1 means respect size hints in tiled resizals */
 
+#include "tatami.c"
 static const Layout layouts[] = {
     /* symbol     arrange function */
-    {"[]=", tile}, /* first entry is default */
-    {"><>", NULL}, /* no layout function means floating behavior */
-    {"[M]", monocle},
+	{ "[]=",      tile },    /* first entry is default */
+	{ "><>",      NULL },    /* no layout function means floating behavior */
+	{ "[M]",      monocle },
+	{ "|+|",      tatami },
 };
 
 /* key definitions */
@@ -95,43 +93,51 @@ static const char *dmenucmd[] = {
 static const char *termcmd[] = {"st", NULL};
 
 static Key keys[] = {
-    /* modifier                     key        function        argument */
-    {MODKEY, XK_p, spawn, {.v = dmenucmd}},
-    {MODKEY | ShiftMask, XK_Return, spawn, {.v = termcmd}},
-    {MODKEY, XK_e, spawn, {.v = eccmd}},
-    {MODKEY, XK_b, togglebar, {0}},
-    {MODKEY, XK_j, focusstackvis, {.i = +1}},
-    {MODKEY, XK_k, focusstackvis, {.i = -1}},
-    {MODKEY | ShiftMask, XK_j, focusstackhid, {.i = +1}},
-    {MODKEY | ShiftMask, XK_k, focusstackhid, {.i = -1}},
-    {MODKEY, XK_i, incnmaster, {.i = +1}},
-    {MODKEY, XK_d, incnmaster, {.i = -1}},
-    {MODKEY, XK_h, setmfact, {.f = -0.05}},
-    {MODKEY, XK_l, setmfact, {.f = +0.05}},
-    {MODKEY, XK_Return, zoom, {0}},
-    {MODKEY, XK_Tab, view, {0}},
-    {MODKEY | ShiftMask, XK_c, killclient, {0}},
-    {MODKEY, XK_t, setlayout, {.v = &layouts[0]}},
-    {MODKEY, XK_f, setlayout, {.v = &layouts[1]}},
-    {MODKEY, XK_m, setlayout, {.v = &layouts[2]}},
-    {MODKEY, XK_space, setlayout, {0}},
-    {MODKEY | ShiftMask, XK_space, togglefloating, {0}},
-    {MODKEY, XK_0, view, {.ui = ~0}},
-    {MODKEY | ShiftMask, XK_0, tag, {.ui = ~0}},
-    {MODKEY, XK_comma, focusmon, {.i = -1}},
-    {MODKEY, XK_period, focusmon, {.i = +1}},
-    {MODKEY | ShiftMask, XK_comma, tagmon, {.i = -1}},
-    {MODKEY | ShiftMask, XK_period, tagmon, {.i = +1}},
-    {MODKEY, XK_n, nametag, {0}},
-    {MODKEY, XK_s, show, {0}},
-    {MODKEY, XK_u, hide, {0}},
-    {MODKEY, XK_q, togglescratch, {.ui = 0}},
-    {MODKEY, XK_w, togglescratch, {.ui = 1}},
-    {MODKEY, XK_r, togglescratch, {.ui = 2}},
-    {MODKEY, XK_v, togglescratch, {.ui = 3}},
-    TAGKEYS(XK_1, 0) TAGKEYS(XK_2, 1) TAGKEYS(XK_3, 2) TAGKEYS(XK_4, 3)
-        TAGKEYS(XK_5, 4) TAGKEYS(XK_6, 5) TAGKEYS(XK_7, 6) TAGKEYS(XK_8, 7)
-            TAGKEYS(XK_9, 8){MODKEY | ShiftMask, XK_q, quit, {0}},
+	/* modifier                     key        function        argument */
+	{ MODKEY,                       XK_p,      spawn,          {.v = dmenucmd } },
+	{ MODKEY|ShiftMask,             XK_Return, spawn,          {.v = termcmd } },
+	{ MODKEY,                       XK_e,      spawn,          {.v = eccmd } },
+	{ MODKEY,                       XK_b,      togglebar,      {0} },
+	{ MODKEY,                       XK_j,      focusstackvis,  {.i = +1 } },
+	{ MODKEY,                       XK_k,      focusstackvis,  {.i = -1 } },
+	{ MODKEY|ShiftMask,             XK_j,      focusstackhid,  {.i = +1 } },
+	{ MODKEY|ShiftMask,             XK_k,      focusstackhid,  {.i = -1 } },
+	{ MODKEY,                       XK_i,      incnmaster,     {.i = +1 } },
+	{ MODKEY,                       XK_d,      incnmaster,     {.i = -1 } },
+	{ MODKEY,                       XK_h,      setmfact,       {.f = -0.05} },
+	{ MODKEY,                       XK_l,      setmfact,       {.f = +0.05} },
+	{ MODKEY,                       XK_Return, zoom,           {0} },
+	{ MODKEY,                       XK_Tab,    view,           {0} },
+	{ MODKEY|ShiftMask,             XK_c,      killclient,     {0} },
+	{ MODKEY,                       XK_t,      setlayout,      {.v = &layouts[0]} },
+	{ MODKEY,                       XK_f,      setlayout,      {.v = &layouts[1]} },
+	{ MODKEY,                       XK_m,      setlayout,      {.v = &layouts[2]} },
+	{ MODKEY,                       XK_y,      setlayout,      {.v = &layouts[3]} },
+	{ MODKEY,                       XK_space,  setlayout,      {0} },
+	{ MODKEY|ShiftMask,             XK_space,  togglefloating, {0} },
+	{ MODKEY|ShiftMask,             XK_f,      togglefullscr,  {0} },
+	{ MODKEY,                       XK_0,      view,           {.ui = ~0 } },
+	{ MODKEY|ShiftMask,             XK_0,      tag,            {.ui = ~0 } },
+	{ MODKEY,                       XK_comma,  focusmon,       {.i = -1 } },
+	{ MODKEY,                       XK_period, focusmon,       {.i = +1 } },
+	{ MODKEY|ShiftMask,             XK_comma,  tagmon,         {.i = -1 } },
+	{ MODKEY|ShiftMask,             XK_period, tagmon,         {.i = +1 } },
+	{ MODKEY,                       XK_n,      nametag,        {0} },
+	{ MODKEY,                       XK_s,      show,           {0} },
+	{ MODKEY,                       XK_u,      hide,           {0} },
+	{ MODKEY,            			XK_q,      togglescratch,  {.ui = 0 } },
+	{ MODKEY,            			XK_w,	   togglescratch,  {.ui = 1 } },
+	{ MODKEY,                       XK_v,      togglescratch,  {.ui = 2 } },
+	TAGKEYS(                        XK_1,                      0)
+	TAGKEYS(                        XK_2,                      1)
+	TAGKEYS(                        XK_3,                      2)
+	TAGKEYS(                        XK_4,                      3)
+	TAGKEYS(                        XK_5,                      4)
+	TAGKEYS(                        XK_6,                      5)
+	TAGKEYS(                        XK_7,                      6)
+	TAGKEYS(                        XK_8,                      7)
+	TAGKEYS(                        XK_9,                      8)
+	{ MODKEY|ShiftMask,             XK_q,      quit,           {0} },
 };
 
 /* button definitions */
